a06242a82b453623a8cddfb0c89a5edfe03b8810
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-08-17 12:58:22 -0700

    walking sim

---
+++ scenes/ground_maker.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+var distance = 0
+
+@onready var ground_scene = load('res://scenes/assets/train_terrain.tscn')
+
+@export var player : Node3D
+@export var starting_length = 8
+@export var spawn_when_within = 5
+
+func _ready():
+	make_railroad()
+# Called when the node enters the scene tree for the first time.
+func make_railroad():
+	for each in get_children():
+		print(each)
+	for x in range(0,starting_length):
+		new_piece()
+		
+
+func new_piece():
+	var new_terrain = ground_scene.instantiate()
+	new_terrain.position = Vector3(0,0,distance*35)
+	add_child(new_terrain)
+	new_terrain.set_owner(self)
+	distance+=1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player.position.z > (distance-spawn_when_within)*35:
+		new_piece()
