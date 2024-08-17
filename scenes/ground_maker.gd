d27d7f96345963bb0c7e39eaf46ea4b0ef22988b
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-08-17 13:57:31 -0700

    effects

--- scenes/ground_maker.gd
+++ scenes/ground_maker.gd
@@ -6,9 +6,15 @@ var distance = 0
 @onready var ground_scene = load('res://scenes/assets/train_terrain.tscn')
 
 @export var player : Node3D
-@export var starting_length = 8
-@export var spawn_when_within = 5
+
+@export var starting_length: int = 8
+@export var spawn_when_within: int = 5
+@export var space_between_parts: int = 35
+
+@export var random_rotation: bool = false
+
 @export var reset : bool = true
+
 func _ready():
 	make_railroad()
 # Called when the node enters the scene tree for the first time.
@@ -21,17 +27,20 @@ func make_railroad():
 
 func new_piece():
 	var new_terrain = ground_scene.instantiate()
-	new_terrain.position = Vector3(0,0,distance*35)
+	new_terrain.position = Vector3(0,0,distance*space_between_parts)
 	new_terrain.name = 'Ground'+str(distance)
+	if randi_range(0,1) == 1 and random_rotation:
+		new_terrain.rotation_degrees.y = 180
 	add_child(new_terrain)
 	new_terrain.set_owner(self)
 	distance+=1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if player.position.z > (distance-spawn_when_within)*35:
+	if player.position.z > (distance-spawn_when_within)*space_between_parts:
 		new_piece()
-		get_child(0).queue_free()
+		if distance > spawn_when_within*2:
+			get_child(0).queue_free()
 	if Engine.is_editor_hint() and reset == true:
 		reset = false
 		distance = 0
