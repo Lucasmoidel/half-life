ceb8b2431835e6d2c92f0d36d8eed4fb678cf57a
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-08-17 17:38:52 -0700

    yo

--- scripts/ground_maker.gd
+++ scripts/ground_maker.gd
@@ -3,9 +3,10 @@ extends Node3D
 
 var distance = 0
 
-@onready var ground_scene = load('res://scenes/assets/train_terrain.tscn')
-@onready var ground_rail_scene = load('res://scenes/assets/rail_scene.tscn')
-#@onready var rail_scene = load('res://scenes/assets/rail_scene.tscn')
+@onready var ground_scene = load('res://scenes/assets/terain2.tscn')
+#@onready var ground_rail_scene = load('res://scenes/assets/rail_scene.tscn')
+@export var rail_scene : PackedScene
+
 @export var player : Node3D
 
 @export var starting_length: int = 8
@@ -27,12 +28,16 @@ func make_railroad():
 		
 
 func new_piece():
-	var new_terrain = ground_rail_scene.instantiate()
+	var new_terrain = ground_scene.instantiate()
+	print(rail_scene)
+	var new_rail = rail_scene.instantiate()
 	new_terrain.position = Vector3(0,0,distance*space_between_parts)
+	new_rail.position = new_terrain.position
 	new_terrain.name = 'Ground'+str(distance)
 	if randi_range(0,1) == 1 and random_rotation:
 		new_terrain.rotation_degrees.y = 180
 	add_child(new_terrain)
+	add_child(new_rail)
 	new_terrain.set_owner(self)
 	distance+=1
 
